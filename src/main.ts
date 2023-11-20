8c49786d58902021dc8d5c1a91024ffc1cfee887
Daulet Tulepbergenov <daulet6368@gmail.com>
2023-11-20 20:38:05 +0600

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder().setTitle('Lockoo App').build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/swagger', app, document);
+
   await app.listen(5000);
 }
 
